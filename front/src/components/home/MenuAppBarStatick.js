c7dc3e7d412cac2d090fd255a7a5398856ceca88
Mecho-sys <nrojazamorano@hotmail.com>
2024-04-24 21:31:19 -0400

    ADD: Form to create new Genogram

--- front/src/components/home/MenuAppBarStatick.js
+++ front/src/components/home/MenuAppBarStatick.js
@@ -14,18 +14,44 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import { DialogContent, DialogActions, TextField, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const MenuAppBarStatick = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const [formData, setFormData] = useState({
     Titulo: "",
+    Abuelo: "",
+    Abuela: "",
+    Childs1: "",
+    Childs2: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleChangeSelect = (fieldName, value) => {
+    setFormData({ ...formData, [fieldName]: value });
   };
 
   const handleNewGen = () => {
@@ -94,9 +120,126 @@ const MenuAppBarStatick = () => {
               Infomación del nucleo familiar
             </AccordionSummary>
             <AccordionDetails>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              <Grid container spacing={2}>
+                <Grid item xs={3}>
+                  <Item variant="outlined">
+                    <FormControlLabel control={<Checkbox />} label="Padre" />
+                  </Item>
+                </Grid>
+                <Grid item xs={3}>
+                  <Item variant="outlined">
+                    <FormControlLabel control={<Checkbox />} label="Madre" />
+                  </Item>
+                </Grid>
+                <Grid item xs={3}>
+                  <Item variant="outlined">
+                    <FormControlLabel control={<Checkbox />} label="Padre" />
+                  </Item>
+                </Grid>
+                <Grid item xs={3}>
+                  <Item variant="outlined">
+                    <FormControlLabel control={<Checkbox />} label="Madre" />
+                  </Item>
+                </Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={4}>
+                  <Item variant="outlined">
+                    <FormControl variant="standard" sx={{ minWidth: 120 }}>
+                      <InputLabel id="chils1-select-label">
+                        Nro Hijos
+                      </InputLabel>
+                      <Select
+                        labelId="chils1-select-label"
+                        id="chils1-select-label"
+                        value={formData.Childs1}
+                        onChange={(e) =>
+                          handleChangeSelect("Childs1", e.target.value)
+                        }
+                        label="Childs1"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Item>
+                </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={4}>
+                  <Item variant="outlined">
+                    <FormControl variant="standard" sx={{ minWidth: 120 }}>
+                      <InputLabel id="chils2-select-label">
+                        Nro Hijos
+                      </InputLabel>
+                      <Select
+                        labelId="chils2-select-label"
+                        id="chils2-select-label"
+                        value={formData.Childs2}
+                        onChange={(e) =>
+                          handleChangeSelect("Childs2", e.target.value)
+                        }
+                        label="Childs2"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Item>
+                </Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={4}>
+                  <Item variant="outlined">
+                    <FormControl variant="standard" sx={{ minWidth: 120 }}>
+                      <InputLabel id="chils1-select-label">
+                        Nro Nietos
+                      </InputLabel>
+                      <Select
+                        labelId="chils1-select-label"
+                        id="chils1-select-label"
+                        value={formData.Childs1}
+                        onChange={(e) =>
+                          handleChangeSelect("Childs1", e.target.value)
+                        }
+                        label="Childs1"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Item>
+                </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={4}>
+                  <Item variant="outlined">
+                    <FormControl variant="standard" sx={{ minWidth: 120 }}>
+                      <InputLabel id="chils2-select-label">
+                        Nro Niertos
+                      </InputLabel>
+                      <Select
+                        labelId="chils2-select-label"
+                        id="chils2-select-label"
+                        value={formData.Childs2}
+                        onChange={(e) =>
+                          handleChangeSelect("Childs2", e.target.value)
+                        }
+                        label="Childs2"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Item>
+                </Grid>
+                <Grid item xs={1}></Grid>
+              </Grid>
             </AccordionDetails>
           </Accordion>
         </DialogContent>
